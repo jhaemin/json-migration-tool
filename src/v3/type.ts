e83fa809ece13af9ab6e41f0cea1a42808665804
Jang Haemin <io@jhaemin.com>
2021-10-08 01:12:17 +0900

    feat: create Primitive class that implements Type

--- src/v3/type.ts
+++ src/v3/type.ts
@@ -12,4 +12,37 @@ export interface Type {
   alias?: string
   buildTsType(): string
   raw(): string
+  isCorrectType(value: unknown): boolean
+}
+
+export class Primitive implements Type {
+  public typeName: 'string' | 'number' | 'boolean' | 'null'
+
+  constructor(public defaultValue?: string | number | boolean | null) {
+    this.typeName = 'null'
+  }
+
+  buildTsType() {
+    return this.typeName
+  }
+
+  raw() {
+    let defaultValue = this.defaultValue === undefined ? '' : this.defaultValue
+
+    if (this.typeName === 'string') {
+      defaultValue = `'${defaultValue}'`
+    }
+
+    return `${this.typeName}(${defaultValue})`
+  }
+
+  isCorrectType(value: unknown) {
+    if (this.typeName === 'null') {
+      return value === null
+    }
+
+    console.log(`value: ${value}, typeof value: ${typeof value}`)
+
+    return typeof value === this.typeName
+  }
 }
