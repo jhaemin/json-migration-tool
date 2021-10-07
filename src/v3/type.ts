a73afb6b816e53f003ee0e8cca64923d37ce40e5
Jang Haemin <io@jhaemin.com>
2021-10-08 01:12:00 +0900

    feat: add default typeName property to Type

--- src/v3/type.ts
+++ src/v3/type.ts
@@ -1,4 +1,14 @@
 export interface Type {
+  typeName:
+    | 'string'
+    | 'number'
+    | 'object'
+    | 'record'
+    | 'array'
+    | 'set'
+    | 'boolean'
+    | 'null'
+    | 'union'
   alias?: string
   buildTsType(): string
   raw(): string
