a534aa8c0a79bf4747e6e75b0d1a38fb7b5ca715
Jang Haemin <io@jhaemin.com>
2021-10-10 00:06:12 +0900

    feat: infer RecordType

--- src/v3/helpers.ts
+++ src/v3/helpers.ts
@@ -1,7 +1,8 @@
 import { NumberType } from './number'
-import { Property, RecordType } from './object'
+import { ObjectType, RecordType } from './object'
 import { StringType } from './string'
 import { Type } from './type'
+import { Union } from './union'
 
 export type UnionToIntersection<U> = (
   U extends any ? (k: U) => void : never
@@ -20,7 +21,7 @@ export type InferType<T extends Type> = T extends StringType
   ? string
   : T extends NumberType
   ? number
-  : T extends RecordType<infer P>
+  : T extends ObjectType<infer Property>
   ? UnionToIntersection<
       {
         [Idx in keyof T['properties']]: Idx extends number
@@ -33,9 +34,36 @@ export type InferType<T extends Type> = T extends StringType
                 : {
                     [K in P['key']]: InferType<P['type']>
                   }
-              : unknown
-            : unknown
-          : unknown
+              : never
+            : never
+          : never
       }[number]
     >
-  : unknown
+  : T extends RecordType<infer Property>
+  ? Record<
+      string,
+      UnionToIntersection<
+        {
+          [Idx in keyof T['properties']]: Idx extends number
+            ? T['properties'][Idx] extends infer P
+              ? P extends Property
+                ? P['optional'] extends true
+                  ? {
+                      [K in P['key']]?: InferType<P['type']>
+                    }
+                  : {
+                      [K in P['key']]: InferType<P['type']>
+                    }
+                : never
+              : never
+            : never
+        }[number]
+      >
+    >
+  : T extends Union<infer TypeArray>
+  ? TypeArray extends (infer T)[]
+    ? T extends Type
+      ? InferType<T>
+      : never
+    : never
+  : never
