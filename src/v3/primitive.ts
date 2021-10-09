07883526dc862ef9a6edb2c3696b86320a5f9438
Jang Haemin <io@jhaemin.com>
2021-10-09 22:41:06 +0900

    refactor: extract Primitive to a dedicated source

---
+++ src/v3/primitive.ts
@@ -0,0 +1,33 @@
+import { Type } from './type'
+
+export class PrimitiveType implements Type {
+  public typeName: 'string' | 'number' | 'boolean' | 'null'
+
+  constructor(public defaultValue?: string | number | boolean | null) {
+    this.typeName = 'null'
+  }
+
+  buildTsType() {
+    return this.typeName
+  }
+
+  raw() {
+    let defaultValue = this.defaultValue === undefined ? '' : this.defaultValue
+
+    if (this.typeName === 'string') {
+      defaultValue = `'${defaultValue}'`
+    }
+
+    return `${this.typeName}(${defaultValue})`
+  }
+
+  isCorrectType(value: unknown) {
+    if (this.typeName === 'null') {
+      return value === null
+    }
+
+    console.log(`value: ${value}, typeof value: ${typeof value}`)
+
+    return typeof value === this.typeName
+  }
+}
