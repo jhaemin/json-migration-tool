3757129f0f3bd59b582f2fab0304c6ccbc57dd8a
Jang Haemin <io@jhaemin.com>
2021-10-13 21:15:19 +0900

    feat: create valueToString module

--- src/v3/types/common.ts
+++ src/v3/types/common.ts
@@ -17,4 +17,35 @@ export interface Type {
   isCorrectType(value: unknown): boolean
 }
 
-export type JsonRuntimeSchema = ObjectLikeType
+export type JsonRuntimeSchema = ObjectType
+
+export function valueToString(value: any) {
+  const type = typeof value
+
+  if (type === 'undefined') {
+    return ''
+  }
+
+  if (type === 'boolean' || type === 'number' || value === null) {
+    return `${value}`
+  }
+
+  if (type === 'string') {
+    return `'${value}'`
+  }
+
+  if (type === 'object') {
+    const properties: string = Object.entries(value)
+      .filter(([, v]) => v !== undefined)
+      .map(([k, v]) => {
+        return `${k}: ${valueToString(v)}`
+      })
+      .join(', ')
+
+    if (properties) {
+      return `{ ${properties} }`
+    }
+  }
+
+  return `'unsupported type'`
+}
